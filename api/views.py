97c44d0baa2f74609fddbc71d5784c15a0d764dc
mansi <mnayak@bestpeers.com>
2024-09-30 18:49:26 +0530

    update and create the views

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+# from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
+from rest_framework import generics, permissions
+from rest_framework.filters import SearchFilter
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -6,10 +9,17 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from api.models import Category, Comment, Follow, Post
-from api.permissions import IsOwnerOrAdmin
-from api.serializers import (CategorySerializer, CommentSerializer,
-                             FollowSerializer, PostSerializer, UserSerializer)
+from api.models import Category, Comment, Follow, Like, Post, User
+from api.serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    FollowSerializer,
+    LikeSerializer,
+    PostSerializer,
+    UserSerializer,
+)
+
+from .permissions import IsOwnerOrAdmin
 
 
 class SignUpView(generics.CreateAPIView):
@@ -94,6 +104,8 @@ class PostListCreateView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_class = PostFilter
 
     def perform_create(self, serializer):
         """Save the post with the current user as the owner"""
@@ -111,6 +123,11 @@ class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [IsOwnerOrAdmin()]
+        return [permissions.IsAuthenticated()]
+
 
 # Comment Views
 
@@ -119,10 +136,14 @@ class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_class = CommentFilter
 
-    def comment_create(self, serializer):
+    def perform_create(self, serializer):
         """Save the comment with the current user as the owner"""
-        serializer.save(user=self.request.user)
+        post_id = self.request.data.get("post")
+        post = Post.objects.get(id=post_id)
+        serializer.save(user=self.request.user, post=post)
 
 
 class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -130,9 +151,12 @@ class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
-    def update(self, serializer):
+    def perform_update(self, serializer):
         """Only allow the comment owner to update it"""
-        if serializer.instance.user == self.request.user or self.request.user.is_staff:
+        if (
+            serializer.instance.user == self.request.user
+            or self.request.user.is_staff
+        ):
             serializer.save()
         else:
             return Response({"detail": "Permission Denied!!"})
@@ -164,11 +188,129 @@ class UserCommentsListView(generics.ListAPIView):
         return Comment.objects.filter(user__id=user_id)
 
 
+# class FollowCreateView(generics.CreateAPIView):
+#     # queryset = Follow.objects.all()
+#     # serializer_class = FollowSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request):
+#         user_following_email = request.POST.get(
+#             "user_following_email"
+#         )  # this is string
+#         user = request.user  # this is user object
+#         user_follow = Follow.objects.create(
+#             user=user, user_following=user_following
+#         )
+
+#         # Check if the email was provided
+#         if not user_following_email:
+#             return Response({"msg": "Provide Email!"})
+
+#         user_following = User.objects.filter(
+#             email=user_following_email
+#         ).first()
+
+#         # Check if the user exists
+#         if not user_following:
+#             return Response({"msg": "No User Found with this Email!"})
+
+#         # check if the user trying to follow themselves
+#         if request.user == user_following:
+#             return Response({"msg": "you cannot follow yourself!"})
+
+#         # check if the person following the same person twice
+#         if Follow.objects.filter(
+#             user=request.user, user_following=user_following
+#         ).exists():
+#             return Response({"msg": "you are already following the user"})
+
+#         return Response(
+#             {
+#                 "msg": f"you are now following {user_following.email}",
+#                 "data": {
+#                     "id": (user_follow.id),
+#                     "user": request.user.email,
+#                     "user_following": user_following.email,
+#                 },
+#             }
+#         )
+
+
+class FollowCreateView(generics.CreateAPIView):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically assign the authenticated user to the 'user' field
+        serializer.save(user=self.request.user)
+
+
 class UserFollowersListView(generics.ListAPIView):
     """Create an API to get all followers of a User"""
 
     serializer_class = FollowSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs["user_id"]
-        return Follow.objects.filter(user_follower_id=user_id)
+        user_id = self.kwargs["user"]
+        # Use `filter` and ensure to handle the case when no followers exist
+        return Follow.objects.filter(user_following_id=user_id)
+
+
+class LikeCreateView(generics.CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get("post_id")
+        post = Post.objects.filter(id=post_id).first()
+
+        if post is None:
+            return Response({"detail": "Post not found."})
+
+        serializer.save(user=self.request.user, post=post)
+
+
+class PostLikeListView(generics.ListAPIView):
+    """create an API to get the list of likes of a user post"""
+
+    serializer_class = LikeSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs["post_id"]
+        return Like.objects.filter(post=post_id)
+
+
+class SearchAPIView(generics.GenericAPIView):
+    filter_backends = [SearchFilter]
+
+    def get(self, request):
+        query = self.request.query_params.get("search", None)
+
+        if query:
+            # search users
+            users = User.objects.filter(Q(email__icontains=query))
+            user_serializer = UserSerializer(users, many=True)
+
+            # search for posts
+            posts = Post.objects.filter(Q(title__icontains=query))
+            post_serializer = PostSerializer(posts, many=True)
+
+            # serach for comment
+            comments = Comment.objects.filter(Q(comment__icontains=query))
+            comment_serializer = CommentSerializer(comments, many=True)
+
+            # combine the results
+            results = {
+                "users": user_serializer.data,
+                "posts": post_serializer.data,
+                "comments": comment_serializer.data,
+            }
+
+            return Response(results)
+        else:
+            return Response({"error": "No search query provided"})
+
+
+#   | (Q(created_at__range=(start_date, end_date)))
