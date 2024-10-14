2ce2ef3057877df79063e06a96f21fbd7455ff97
mansi <mnayak@bestpeers.com>
2024-10-14 12:29:25 +0530

    update the serializer

--- api/serializers.py
+++ api/serializers.py
@@ -16,25 +16,51 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ["id", "category_name", "description"]
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "category_name", "description"]
 
+    def validate_category_name(self, value):
+        if not value:
+            raise serializers.ValidationError("Category name cannot be empty.")
+
+        # Check if the value contains only alphabetic characters and spaces
+        for char in value:
+            if not (char.isalpha() or char.isspace()):
+                raise serializers.ValidationError(
+                    "Category name should only contain letters and spaces."
+                )
+
+        return value
+
 
 class CommentSerializer(serializers.ModelSerializer):
-    post = serializers.StringRelatedField()
-    user = serializers.StringRelatedField()
+    post_id = serializers.PrimaryKeyRelatedField(
+        queryset=Post.objects.all(), write_only=True, source="post"
+    )
 
     class Meta:
         model = Comment
-        fields = ["id", "comment", "user", "post"]
+        fields = ["id", "comment", "post"]
+
+    def create(self, validated_data):
+        # Automatically assign the user when creating the comment
+        user = self.context["request"].user
+        validated_data["user"] = user
+        return super().create(validated_data)
 
 
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     image = serializers.ImageField(max_length=None, use_url=True)
-    category = serializers.StringRelatedField()  # Accept category ID for input
+    category = serializers.StringRelatedField()
 
     class Meta:
         model = Post
@@ -43,44 +69,53 @@ class PostSerializer(serializers.ModelSerializer):
 
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-    user_following = serializers.StringRelatedField()
+    user_following_email = serializers.EmailField(
+        source="user_following", write_only=True
+    )
 
     class Meta:
         model = Follow
         fields = [
             "id",
             "user",
-            "user_following",
-        ]  # Changed user_followed to user_following
+            "user_following_email",
+        ]
 
     def validate(self, data):
         """Ensure that the user cannot follow themselves or the same user twice."""
         user = self.context["request"].user
-        # The user being followed
-        followed_user_email = data.get("user_following")
+        # The email of the user being followed
+        followed_user_email = data.get("user_following", None)
 
-        # Check if the user trying to follow itself
+        # Check if the user is trying to follow themselves
         if user.email == followed_user_email:
             raise serializers.ValidationError("You cannot follow yourself!")
 
-        # Check if the user trying to follow the same user twice
-        if Follow.objects.filter(
-            user=user, user_following__email=followed_user_email
-        ).exists():
+        # Check if the user is trying to follow the same user twice
+        following_user = User.objects.filter(email=followed_user_email).first()
+        if (
+            following_user
+            and Follow.objects.filter(
+                user=user, user_following=following_user
+            ).exists()
+        ):
             raise serializers.ValidationError(
                 "You are already following this user."
             )
+
+        data["user_following"] = (
+            following_user  # Assign the user instance to user_following
+        )
 
         return data
 
 
 class LikeSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-    # Read-only representation of the post
     post = serializers.StringRelatedField(read_only=True)
     post_id = serializers.PrimaryKeyRelatedField(
         queryset=Post.objects.all(), write_only=True, source="post"
-    )  # Write-only representation of the post
+    ) 
 
     class Meta:
         model = Like
